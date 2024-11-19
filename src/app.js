82c920c0cf7622a292b2824e83649c5efa54724d
Sachin-Balagam <b.sachin@fincity.com>
2024-11-19 23:18:33 +0530

    Exploring GET,PUT,PATCH,DELETE Methods

--- src/app.js
+++ src/app.js
@@ -2,7 +2,27 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res) => {
+app.get("/user", (req, res) => {
+  res.send({ firstName: "Sachin", lastName: "Balagam" });
+});
+
+app.post("/user", (req, res) => {
+  res.send("Full Data Updated Successfully");
+});
+
+app.patch("/user", (req, res) => {
+  res.send("Data Updated Successfully");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("Data Deleted");
+});
+
+app.use("/test", (req, res) => {
+  res.send("Test Test Test...");
+});
+
+app.use("/get", (req, res) => {
   res.send("Response from Test Server!!!");
 });
 
