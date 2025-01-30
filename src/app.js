a12ea4c22cddd6fcbb89b594ef844ea6ce3a8446
SachinBalagam <balagamsachin337@gmail.com>
2025-01-30 23:08:55 +0530

    new APIs added

--- src/app.js
+++ src/app.js
@@ -5,25 +5,43 @@ import { UserModel } from "./models/user.js";
 import express from "express";
 const app = express();
 
+app.use(express.json());
+
 app.post("/signup", async (req, res) => {
+  const { firstName, age, emailId } = req.body;
+  const userObj = {
+    firstName,
+    age,
+    emailId,
+  };
   try {
-    const userObj = {
-      firstName: "Sachin Kumar",
-      lastName: "Balagam",
-      emailId: "sachin333@gmail.com",
-    };
     const user = new UserModel(userObj);
     await user.save();
     res.send("User Saved Successfully");
   } catch (err) {
     console.error("Error Message", err);
   }
+});
 
-  //   const user = new UserModel({
-  //     firstName: "Sachin",
-  //     lastName: "Balagam",
-  //     emailId: "sachin@gmail.com",
-  //   });
+//   const user = new UserModel({
+//     firstName: "Sachin",
+//     lastName: "Balagam",
+//     emailId: "sachin@gmail.com",
+//   });
+
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.email;
+  try {
+    const user = await UserModel.find({ emailId: userEmail });
+    if (user.length >= 1) {
+      res.send(user);
+    } else {
+      res.status(404).send("User not found");
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(401).send("Something went wrong");
+  }
 });
 
 (async () => {
